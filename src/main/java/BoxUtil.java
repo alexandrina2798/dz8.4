20a137f9552c959f4a29d49dc42e1d697d242c7b
alexandrina2798 <alexandrina2798@mail.ru>
2021-03-18 18:20:40 +0300

    fixed warnings

--- src/main/java/BoxUtil.java
+++ src/main/java/BoxUtil.java
@@ -5,9 +5,7 @@ public class BoxUtil {
     // скопировать содержимое из одной коробки в другую
     //Box(dest) в которую будем копировать может быть типизирована любым родителем объекта содержащимся в Box(src)
     public static <T> void copyFromBoxToBox(Box<T> src, Box<T> dest) {
-        Box <T> box1 = src;
-        Box <T> box2 = dest;
-        box2.put(box1.get());
+        dest.put(src.get());
     }
 
     // скопировать содержимое из Box(src) которая может быть типизирована только классом Fruit и его наследниками,
@@ -15,9 +13,8 @@ public class BoxUtil {
     //Box(dest) в которую будем копировать может быть типизирована любым родителем объекта содержащимся в Box(src)
     public static <T> void copyFreshFruitFromBoxToBox(Box<T> src, Box<T> dest) {
         Box <Fruit> box1 = (Box<Fruit>) src;
-        Box <T> box2 = dest;
         if (box1.get().getFresh()){
-            box2.put((T) box1.get());
+            dest.put((T) box1.get());
         }
     }
 
